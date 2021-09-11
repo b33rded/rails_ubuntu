288950074250739751c4c247858a0eba7a51414d
83312d <warrrgh666@gmail.com>
2021-09-11 19:13:54 +0300

    attributes/def.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-default['rails_ubuntu']['deploy_user']    = 'vagrant'
+default['rails_ubuntu']['deploy_user']    = 'deploy'
 # default['rails_ubuntu']['deploy_group'] = '<deploy_user>'
 # default['rails_ubuntu']['deploy_home']  = '$HOME'
 
-default['rails_ubuntu']['ruby_version']   = '2.7.1'
+default['rails_ubuntu']['ruby_version']   = '2.6.7'
 default['rails_ubuntu']['node_version']   = '12'
 default['rails_ubuntu']['proxysql_version'] = '2.0'
 
@@ -16,14 +16,14 @@ default['rails_ubuntu']['server_name']    = node['fqdn']
 default['rails_ubuntu']['app_type']       = 'rails'   # rails | node
 default['rails_ubuntu']['app_env']        = 'production'
 default['rails_ubuntu']['app_startup']    = 'app.js'  # Node app boot file.
-default['rails_ubuntu']['app_name']       = 'myapp'
+default['rails_ubuntu']['app_name']       = 'testapp'
 # default['rails_ubuntu']['nginx_site']   = app_name
 # default['rails_ubuntu']['deploy_to']    = '<deploy_home>/<app_name>'
 # default['rails_ubuntu']['app_root']     = '<deploy_to>/current'
 # default['rails_ubuntu']['app_public']   = '<app_root>/public'
 
 # Local database server.
-default['rails_ubuntu']['db_type']        = 'none' # none | postgres | mysql
+default['rails_ubuntu']['db_type']        = 'postgres' # none | postgres | mysql
 # default['rails_ubuntu']['db_user']      =
 # default['rails_ubuntu']['db_password']  =
 # default['rails_ubuntu']['db_name']      =
